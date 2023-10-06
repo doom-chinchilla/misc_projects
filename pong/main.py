90723f6244ca23c80fea3b89b8d610726ef4ebec
Alexandra Noreiga <112288593+doom-chinchilla@users.noreply.github.com>
2023-10-06 09:45:52 -0500

    starting pong

---
+++ pong/main.py
@@ -0,0 +1,15 @@
+import turtle 
+
+wn = turtle.Screen()
+
+wn.title("Ping Pong!")
+wn.bgcolor("black")
+wn.setup(width=800,height=800)
+wn.tracer(0)
+
+# creating paddle A 
+
+
+# start the main game loop 
+while True: 
+    wn.update()
